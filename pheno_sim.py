5848e4403474fa440251e676db99b13852d9d940
KesarChi <momiji0613@163.com>
2026-07-19 21:32:36 +0800

    Update pheno_sim.py

--- pheno_sim.py
+++ pheno_sim.py
@@ -1,3 +1,20 @@
+"""
+To generate simulated phenotypes with realistic genetic architectures, we constructed individual-level BMI and T2D phenotypes based on empirical PRS. 
+The observed BMI-PRS and T2D-PRS values were standardized to have a mean of zero and a variance of one before simulation. 
+For BMI, we assumed a SNP-based heritability ((h2)) of 0.50 and specified that the empirical BMI-PRS explained 18% of the total phenotypic variance ((R2 of PRS=0.18)). 
+The PRS contribution was modeled as: GPRS=PRSBMI×(R2 of PRS)1/2, so that the variance explained by the PRS component was equal to the predefined (R2 of PRS). 
+The remaining genetic component not captured by the PRS was simulated independently from a normal distribution with variance: Var(Gresidual)=h2-(R2 of PRS), representing additional genetic effects from variants not included or captured by the PRS. 
+Environmental and unexplained residual effects were simulated independently with variance: 1-h2. 
+The latent BMI liability was then generated as the sum of the PRS-derived genetic component, residual genetic component, and environmental component: LBMI=GPRS+Gresidual+E.
+The resulting continuous liability score was transformed to an observed BMI scale using population parameters from European adults (mean BMI = 27.3 kg/m2 and standard deviation = 4.75 kg/m2): BMI=LBMI×4.75+27.3.
+
+For T2D, we applied a similar liability-based framework. We assumed a total heritability of 0.50 and a PRS-explained variance of 0.10 (R2 of PRS=0.10). 
+The residual genetic component was simulated with variance: Var(Gresidual)=h2-(R2 of PRS), and non-genetic variance (1-h2), respectively. 
+To convert the continuous liability score into a binary disease phenotype, we applied a liability-threshold model. 
+The resulting liability score was converted into binary disease status using a liability-threshold model calibrated to a population prevalence of 12%. 
+Individuals with liability scores exceeding this threshold were classified as T2D cases, while all remaining individuals were classified as controls. 
+"""
+
 import numpy as np
 from scipy import stats
 import pandas as pd
